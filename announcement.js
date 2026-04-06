2d7cbccd640972a20ce398aaf1a4bd9186d56ad8
chubin <befanyelse@gmail.com>
2026-04-06 19:52:26 +0900

    [0406] 양수빈-JStest

---
+++ announcement.js
@@ -0,0 +1,40 @@
+// 1. 2
+// 2. console.log(fruits[1]);
+// 3. 짝수입니다.
+// 4. NaN NaN
+// 5. style.display += 1
+// 6.
+// 7.let answer = 0;
+// const isLogin = true;
+// answer = isLogin ? "환영합니다" : "접근 권한이 없습니다.";
+// consosle.log(answer)
+// 
+// 8. let userInput;
+// do {
+//   console.log(promt("무엇을 말하고 싶나요?"));
+// } while ( userInput === "끝");
+//
+// 9. 
+//
+// 10. 
+// a = new
+// arr = [old, 10, 233, true, new]
+// b : a = true
+// arr = [old, 10, 233]
+// 
+// 11. createElementByClassName
+// 12. 
+
+// person.push("hobby:drawing")
+
+//13.
+
+//14. 
+//document.getElementById
+//addEventListener
+//click
+
+let person = {
+  name: "lion"
+}
+
